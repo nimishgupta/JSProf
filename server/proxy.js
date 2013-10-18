15a91d4c6767238457ede9bc797375b6cd147369
nimishgupta <nimish28@gmail.com>
2013-10-18 02:40:51 -0400

    working proxy that is able to detect javascript both file and included code

--- server/proxy.js
+++ server/proxy.js
@@ -1,21 +1,91 @@
-var http = require ('http');
+var http    = require ('http');
+var request = require ('request');
+var cheerio = require ('cheerio');
+var url     = require ('url');
 
-http.createServer(function(request, response) {
-  var proxy = http.createClient(80, request.headers['host'])
-  var proxy_request = proxy.request(request.method, request.url, request.headers);
-  proxy_request.addListener('response', function (proxy_response) {
-    proxy_response.addListener('data', function(chunk) {
-      response.write(chunk, 'binary');
-    });
-    proxy_response.addListener('end', function() {
-      response.end();
-    });
-    response.writeHead(proxy_response.statusCode, proxy_response.headers);
-  });
-  request.addListener('data', function(chunk) {
-    proxy_request.write(chunk, 'binary');
-  });
-  request.addListener('end', function() {
-    proxy_request.end();
-  });
-}).listen(8080);
+
+
+function process_script_tag (index, element)
+{
+  if (element.attr ('src'))
+  {
+    console.log ("src is : " + element.attr ('src'));
+    
+
+    // use url.resolve to resolve script references
+  }
+  else
+  {
+    console.log ("javascript is : " + element.html ());
+  }
+}
+
+
+function remote_response_handler (error,
+                                  remote_response, 
+                                  body, 
+                                  local_response)
+{
+  local_response.writeHead (remote_response.statusCode,
+                            remote_response.headers);
+
+  if (!error && remote_response.statusCode === 200)
+  {
+    // AOK!
+
+    console.log (remote_response.headers['content-type']);
+    var html = new RegExp ("html");
+    if (true ===  html.test (remote_response.headers['content-type']))
+    {
+      // parse html
+      console.log ("Here");
+      console.log (body);
+      $ = cheerio.load (body);
+      $('script').each (function (index, element)
+                        {
+                          process_script_tag (index, $(this));
+                        });
+    }
+    else
+    {
+      console.log ("regex failed");
+    }
+  }
+
+  local_response.end (body, encoding='binary');
+}
+
+
+
+
+function JSProf_server (local_request, local_response)
+{
+  var html = new RegExp ("html");
+  if (true ===  html.test (local_request.headers.accept))
+  {
+    if (local_request.headers['accept-encoding'])
+    {
+      delete local_request.headers['accept-encoding'];
+    }
+
+    console.log (local_request.url);
+
+    request (local_request,
+             function (error, remote_response, body)
+             {
+               remote_response_handler (error, 
+                                        remote_response,
+                                        body,
+                                        local_response);
+             });
+  }
+  else
+  {
+    local_request.pipe (request (local_request.url)).pipe (local_response);
+  }
+}
+
+
+
+
+http.createServer (JSProf_server).listen (2500);
